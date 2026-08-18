64910cf6cdb36e63cd247fa8966121877776e41c
almubarky05588-lab <almubarky05588@gmail.com>
2026-08-19 01:47:29 +0300

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,12 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // استقبال الإشعارات وعرضها أثناء فتح التطبيق
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate =
+        self as? UNUserNotificationCenterDelegate
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
